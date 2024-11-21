642159b4d88ce4295b42fa12fc22508a8b26a2d5
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-20 21:40:45 -0300

    Finalizando CSS no React.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Car from './components/Car';
 
 function App() {
+  const myCars = [
+    { name: 'Fusca', km: 10000, color: 'Branca'},
+    { name: 'Polo', km: 32000, color: 'Cinza'},
+    { name: 'Onix', km: 0, color: 'Preto'},
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Show Room of Cars</h1>
+      <div className="car_container">
+        {myCars.map((car) => (
+          <Car car={car}/>
+        ))}
+      </div>
+      
     </div>
   );
 }
